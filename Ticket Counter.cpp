d2c1bfbf29441001ca30724e98916f129f8a32f4
Samarth Mishra <91658673+Samarth277@users.noreply.github.com>
2023-06-18 22:14:43 +0530

    Ticket Counter #PODT 18-june

---
+++ Ticket Counter.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int distributeTicket(int N, int K) {
+        // code here
+        int st=1; int end=N;
+        while(st<end)
+        {
+            int time=K;
+            while(st<end && time-- >0) st++;
+            time=K;
+            while(st<end && time-- >0) end--;
+            
+        }
+        return st;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        int K;
+        scanf("%d",&K);
+        
+        Solution obj;
+        int res = obj.distributeTicket(N, K);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
